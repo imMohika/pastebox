41ab07380336a026f1c091821a802a153506237d
immohika <mohi222@pm.me>
2024-10-21 10:33:53 +0330

    graceful shutdown

--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	"context"
 	"database/sql"
 	_ "embed"
+	"errors"
 	"flag"
+	"fmt"
+	"io"
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
 	"pastebox.mohika.ir/internal/database"
 	"pastebox.mohika.ir/web"
+	"syscall"
 	"time"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -19,30 +24,37 @@ import (
 var ddl string
 
 func main() {
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-
 	addr := flag.String("addr", ":8080", "http service address")
 	dsn := flag.String("dsn", ":memory:", "SQLITE data source name")
 	flag.Parse()
 
-	db, err := openDB(*dsn)
-	if err != nil {
-		logger.Error(err.Error())
+	ctx := context.Background()
+	if err := run(ctx, os.Stdout, *addr, *dsn); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context, writer io.Writer, addr string, dsn string) error {
+	ctx, cancel := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(dsn)
+	if err != nil {
+		return err
 	}
 	defer db.Close()
 
-	ctx := context.Background()
 	if _, err := db.ExecContext(ctx, ddl); err != nil {
-		logger.Error(err.Error())
-		os.Exit(1)
+		return err
 	}
 	queries := database.New(db)
 
 	templateCache, err := web.NewTemplateCache()
 	if err != nil {
-		logger.Error(err.Error())
-		os.Exit(1)
+		return err
 	}
 
 	server := &web.Server{
@@ -52,18 +64,30 @@ func main() {
 		TemplateCache: templateCache,
 	}
 
-	logger.Info("Starting server", "addr", *addr)
-
 	httpServer := &http.Server{
-		Addr:              *addr,
+		Addr:              addr,
 		ReadHeaderTimeout: 3 * time.Second,
 		Handler:           server.Routes(),
 	}
-	err = httpServer.ListenAndServe()
-	if err != nil {
-		logger.Error(err.Error())
-		os.Exit(1)
+
+	errChan := make(chan error, 1)
+	go func() {
+		logger.InfoContext(ctx, "server started", "addr", addr)
+		if err := httpServer.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			errChan <- err
+		}
+	}()
+
+	select {
+	case err := <-errChan:
+		return err
+	case <-ctx.Done():
+		logger.InfoContext(ctx, "server stopped")
 	}
+
+	ctx, cancel = context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	return httpServer.Shutdown(ctx)
 }
 
 func openDB(dsn string) (*sql.DB, error) {
